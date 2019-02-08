504f4ec451d3f63ffb4be29dfb78487ccb34e6b3
Otto van der Schaaf <oschaaf@we-amp.com>
2019-02-08 12:12:26 +0100

    Whoops, fix a bug that crept into do_ci.sh
    Signed-off-by: Otto van der Schaaf <oschaaf@we-amp.com>

--- ci/do_ci.sh
+++ ci/do_ci.sh
@@ -1,10 +1,5 @@
 #!/bin/bash -e
 
-if [[ -f "${HOME:-/root}/.gitconfig" ]]; then
-    #mv "${HOME:-/root}/.gitconfig" "${HOME:-/root}/.gitconfig_save"
-    echo 1
-fi
-
 function do_build () {
     bazel build --verbose_failures=true //nighthawk:nighthawk_client
 }
@@ -18,14 +13,18 @@ function do_clang_tidy() {
     ci/run_clang_tidy.sh
 }
 
-# TODO(oschaaf): hack, this should be done in .circleci/config.yml	# TODO(oschaaf): To avoid OOM kicking in, we throttle resources here. Revisit this later
+# TODO(oschaaf): hack, this should be done in .circleci/config.yml
 git submodule update --init --recursive
 
 # TODO(oschaaf): To avoid OOM kicking in, we throttle resources here. Revisit this later
 # to see how this was finally resolved in Envoy's code base. There is a TODO for when
 # when a later bazel version is deployed in CI here:
 # https://github.com/lizan/envoy/blob/2eb772ac7518c8fbf2a8c7acbc1bf89e548d9c86/ci/do_ci.sh#L86
-if [ -z "$CIRCLECI" ]; then
+if [ -n "$CIRCLECI" ]; then
+    if [[ -f "${HOME:-/root}/.gitconfig" ]]; then
+        mv "${HOME:-/root}/.gitconfig" "${HOME:-/root}/.gitconfig_save"
+        echo 1
+    fi
     export BAZEL_BUILD_OPTIONS="${BAZEL_BUILD_OPTIONS} --local_resources=4096,2,1"
     export BAZEL_TEST_OPTIONS="${BAZEL_TEST_OPTIONS} --local_resources=4096,2,1 --local_test_jobs=4"
     export PATH=/usr/lib/llvm-7/bin:$PATH
